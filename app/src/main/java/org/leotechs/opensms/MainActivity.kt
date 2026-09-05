1434da1e03cb5488a960962b6e521aaf3d2aafb4
leo-mid <leomidili0@gmail.com>
2026-09-04 23:24:13 -0400

    Added the necessary listeners and permission requests to access sms messages

--- app/src/main/java/org/leotechs/opensms/MainActivity.kt
+++ app/src/main/java/org/leotechs/opensms/MainActivity.kt
@@ -1,7 +1,9 @@
 package org.leotechs.opensms
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.provider.Telephony
 import android.widget.Toast
@@ -19,10 +21,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import org.leotechs.opensms.ui.theme.OpenSMSTheme
 
 class MainActivity : ComponentActivity() {
     private val REQUEST_DEFAULT_APP = 101
+    private val MY_PERMISSIONS_REQUEST_SMS = 102
     private var isDefaultSmsApp by mutableStateOf(false)
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,6 +39,8 @@ class MainActivity : ComponentActivity() {
             val setSmsAppIntent = Intent(Telephony.Sms.Intents.ACTION_CHANGE_DEFAULT)
             setSmsAppIntent.putExtra(Telephony.Sms.Intents.EXTRA_PACKAGE_NAME, packageName)
             startActivityForResult(setSmsAppIntent, REQUEST_DEFAULT_APP)
+        } else {
+            checkSmsPermissions()
         }
 
         enableEdgeToEdge()
@@ -50,17 +57,51 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    private fun checkSmsPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_MMS) != PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_WAP_PUSH) != PackageManager.PERMISSION_GRANTED) {
+            
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.READ_SMS,
+                    Manifest.permission.RECEIVE_SMS,
+                    Manifest.permission.SEND_SMS,
+                    Manifest.permission.RECEIVE_MMS,
+                    Manifest.permission.RECEIVE_WAP_PUSH),
+                MY_PERMISSIONS_REQUEST_SMS)
+        }
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_DEFAULT_APP) {
             if (resultCode == Activity.RESULT_OK) {
                 isDefaultSmsApp = true
                 Toast.makeText(this, "OpenSMS is now your default SMS app!", Toast.LENGTH_SHORT).show()
+                checkSmsPermissions()
             } else {
                 isDefaultSmsApp = false
                 Toast.makeText(this, "Permission denied. App may not function correctly.", Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        val granted = if(checkPermissionGranted(requestCode, permissions, grantResults)) "permission granted" else "permission not granted"
+        Toast.makeText(this, granted, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun checkPermissionGranted(requestCode: Int, permissions: Array<String>, grantResults: IntArray): Boolean{
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_SMS -> {
+                // Return true if all permissions are granted
+                return grantResults.isNotEmpty() && grantResults.all { it == PackageManager.PERMISSION_GRANTED }
+            }
+        }
+        return false
     }
 }
 

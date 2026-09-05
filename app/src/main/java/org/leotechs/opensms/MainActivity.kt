e07bda8ca9874dd1f99bf75053aadaa68bd23430
leo-mid <leomidili0@gmail.com>
2026-09-05 04:33:26 -0400

    Added the word messages to the conversation list

--- app/src/main/java/org/leotechs/opensms/MainActivity.kt
+++ app/src/main/java/org/leotechs/opensms/MainActivity.kt
@@ -38,6 +38,7 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -222,6 +223,12 @@ fun ConversationList(
     }
 
     Column(modifier = modifier) {
+        Text(
+            text = "Messages",
+            style = MaterialTheme.typography.headlineLarge,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(16.dp)
+        )
         if (!isDefault) {
             Button(
                 onClick = onRequestDefault,

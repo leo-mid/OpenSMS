68210310e72ed8dcf663a245961f4c845c3b76d7
leo-mid <leomidili0@gmail.com>
2026-09-06 14:51:21 -0400

    Updated to version v1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 29
         targetSdk = 37
         versionCode = 1
-        versionName = "v1.1.1-beta"
+        versionName = "v1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
